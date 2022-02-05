9f560b7f7947078ec8c2fbbf50cd18efefdb47a3
kit-kiten <kit.kiten01@gmail.com>
2022-02-06 02:10:41 +1000

    change toDoList

--- toDoList/main.js
+++ toDoList/main.js
@@ -1,43 +1,81 @@
-const list = {}
+const STATUSES = {
+    TO_DO: 'To Do',
+    IN_PROGRESS: 'In Progress',
+    DONE: 'Done'
+}
+
+const PRIORITIES = {
+    LOW: 'low',
+    HIGH: 'high'
+}
+
+const list = []
 
 function changeStatus(task, status) {
-    const checkTask = task in list
-    const checkStatus = status === 'To Do' || status === 'In Progress' || status === 'Done'
-    if (checkTask && checkStatus){
-        list[task] = status
+    const checkTask = list.findIndex(item => item.name === task)
+    const checkStatus = status === STATUSES.TO_DO || status === STATUSES.IN_PROGRESS || status === STATUSES.DONE
+    if (checkTask !== -1 && checkStatus) {
+        list.find(item => item.name === task).status = status
+    }
+}
+
+function changePriority(task, priority) {
+    const checkTask = list.findIndex(item => item.name === task)
+    const checkPriority = priority === PRIORITIES.LOW || priority === PRIORITIES.HIGH
+    if (checkTask !== -1 && checkPriority) {
+        list.find(item => item.name === task).priority = priority
     }
 }
 
 function addTask(task) {
-    list[task] = 'To Do'
+    list.push({
+        'name': task,
+        'status': STATUSES.TO_DO,
+        'priority': PRIORITIES.LOW
+    })
 }
 
 function deleteTask(task) {
-    if (task in list){
-        delete list[task]
+    let indexTask = list.findIndex(item => item.name === task)
+    if (indexTask !== -1) {
+        list.splice(indexTask, 1)
     }
 }
 
-function showList() {
-    let toDo = '', inProgress = '', done = ''
-    for (let task in list){
-        if (list[task] === 'To Do'){
-            toDo += `\n "${task}",`
-        } else if (list[task] === 'In Progress'){
-            inProgress += `\n "${task}",`
-        } else{
-            done += `\n "${task}",`
+function showBy(whatShow) {
+    if (whatShow === 'status') {
+        let toDo = '', inProgress = '', done = ''
+        for (let task of list) {
+            if (task.status === STATUSES.TO_DO) {
+                toDo += `\n "${task.name}",`
+            } else if (task.status === STATUSES.IN_PROGRESS) {
+                inProgress += `\n "${task.name}",`
+            } else {
+                done += `\n "${task.name}",`
+            }
         }
+        console.log(`Todo:${toDo || '\n -'}`)
+        console.log(`In Progress:${inProgress || '\n -'}`)
+        console.log(`Done:${done || '\n -'}`)
+    } else if (whatShow === 'priority') {
+        let low = '', high = ''
+        for (let task of list) {
+            if (task.priority === PRIORITIES.LOW) {
+                low += `\n "${task.name}",`
+            } else if (task.priority === PRIORITIES.HIGH) {
+                high += `\n "${task.name}",`
+            }
+        }
+        console.log(`low:${low || '\n -'}`)
+        console.log(`high:${high || '\n -'}`)
     }
-    console.log(`Todo:${toDo || '\n -'}`)
-    console.log(`In Progress:${inProgress || '\n -'}`)
-    console.log(`Done:${done || '\n -'}`)
+
 }
 
 addTask('First task')
 addTask('Second task')
 addTask('Third task')
-deleteTask('Third task')
-changeStatus('Second task', 'In Progress')
+changeStatus('Third task', 'Done')
 
-showList()
+showBy('status')
+showBy('priority')
