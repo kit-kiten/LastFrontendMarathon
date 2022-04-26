89ccb45b15586386f055adfd675d935b9dfbccbc
kit-kiten <malysnikitaenko@gmail.com>
2022-04-26 22:05:26 +1000

    add functional in chat app

--- chat/main.mjs
+++ chat/main.mjs
@@ -1,11 +1,29 @@
 import {UI_ELEMENTS} from "./view.mjs";
 
+function createMessageElementUI(){
+    const messageSubmit = document.querySelector('#message_submit')
+    const li = document.createElement('li')
+    li.className = 'dialog__personal_message dialog__message dialog__delivered_message'
+    li.append(messageSubmit.content.cloneNode(true))
+    li.firstChild.textContent = 'Я: ' + UI_ELEMENTS.MESSAGE_INPUT.value
+    UI_ELEMENTS.MESSAGES_LIST.append(li)
+    li.scrollIntoView(false)
+}
+
 UI_ELEMENTS.BTN_SETTINGS.addEventListener('click', () => {
-    UI_ELEMENTS.DIALOG_BLOCK.style.display = 'none'
     UI_ELEMENTS.SETTINGS_BLOCK.style.display = 'flex'
 })
 
 UI_ELEMENTS.BTN_CLOSE_SETTINGS.addEventListener('click', () => {
     UI_ELEMENTS.SETTINGS_BLOCK.style.display = 'none'
-    UI_ELEMENTS.DIALOG_BLOCK.style.display = 'block'
+})
+
+UI_ELEMENTS.MESSAGE_FORM.addEventListener('submit', () => {
+    const isEmptyMessageInput = UI_ELEMENTS.MESSAGE_INPUT.value === ''
+
+    if (!isEmptyMessageInput){
+        createMessageElementUI()
+    }
+
+    UI_ELEMENTS.MESSAGE_INPUT.value = ''
 })
--- chat/view.mjs
+++ chat/view.mjs
@@ -2,5 +2,7 @@ export const UI_ELEMENTS = {
     BTN_SETTINGS: document.querySelector('.dialog__btn-settings'),
     BTN_CLOSE_SETTINGS: document.querySelector('.settings__close'),
     SETTINGS_BLOCK: document.querySelector('.settings'),
-    DIALOG_BLOCK: document.querySelector('.dialog')
+    MESSAGE_FORM: document.querySelector('.dialog__bottom'),
+    MESSAGES_LIST: document.querySelector('.dialog__message-list'),
+    MESSAGE_INPUT: document.querySelector('.dialog__message-input')
 }
