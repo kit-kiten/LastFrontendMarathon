66eaa7ae14590b0ab49c2631a2cd21124229be09
kit-kiten <malysnikitaenko@gmail.com>
2022-04-27 00:11:32 +1000

    refactoring code in chat app

--- chat/main.mjs
+++ chat/main.mjs
@@ -1,11 +1,14 @@
 import {UI_ELEMENTS} from "./view.mjs";
+import { format } from 'date-fns';
 
 function createMessageElementUI(){
     const messageSubmit = document.querySelector('#message_submit')
     const li = document.createElement('li')
     li.className = 'dialog__personal_message dialog__message dialog__delivered_message'
     li.append(messageSubmit.content.cloneNode(true))
-    li.firstChild.textContent = 'Я: ' + UI_ELEMENTS.MESSAGE_INPUT.value
+    li.querySelector('.dialog__message-text').textContent = 'Я: ' + UI_ELEMENTS.MESSAGE_INPUT.value
+    li.querySelector('.dialog__message-time').textContent = String(format(new Date(), 'HH:mm'))
+    console.log(li.childNodes)
     UI_ELEMENTS.MESSAGES_LIST.append(li)
     li.scrollIntoView(false)
 }
