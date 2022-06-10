e3e5e628bf72bc29374da87ab6c686d0e1c69dbb
kit-kiten <malysnikitaenko@gmail.com>
2022-06-11 01:35:46 +1000

    dev app

--- to-do-list-react/src/App.jsx
+++ to-do-list-react/src/App.jsx
@@ -4,11 +4,11 @@ import './App.css'
 function Task() {
   return (
       <li className={'main-task'}>
-        <div className="main-task__box">
-          <span className="main-task__checkbox"></span>
-        </div>
-        <p className="main-task__text">Таск</p>
-        <input className="main-task__btn" type="button" />
+         <div className="main-task__box">
+             <span className="main-task__checkbox"></span>
+         </div>
+         <p className="main-task__text">Таск</p>
+         <input className="main-task__btn" type="button" />
       </li>
   )
 }
@@ -22,14 +22,21 @@ function List(props) {
 }
 
 function Header(props) {
-  return (
-      <form className="main-form">
-        <div className="main-new_task">
-          <input className="main-new_task__text" type="text" placeholder={props.placeholder_value} />
-          <button className="main-new_task__btn" type="submit"></button>
-        </div>
-      </form>
-  )
+    const [currentInput, setCurrentInput] = useState('')
+
+    function changeInput(e){
+        setCurrentInput(e.target.value)
+        console.log(currentInput)
+    }
+
+    return (
+       <form className="main-form">
+          <div className="main-new_task">
+            <input className="main-new_task__text" type="text" placeholder={props.placeholder_value} onChange={changeInput}/>
+            <button className="main-new_task__btn" type="submit"></button>
+          </div>
+       </form>
+    )
 }
 
 function Title(props) {
@@ -38,31 +45,36 @@ function Title(props) {
   )
 }
 
-function App() {
-
-  return (
-      <main className="main">
-        <div className="container">
-          <Title status={'high'} />
-
-          <Header placeholder_value={'Добавить важных дел'} />
-
-          <List>
-            <Task />
-            <Task />
-          </List>
-
-          <Title status={'low'} />
-
-          <Header placeholder_value={'Добавить'}/>
-
-          <List>
-            <Task />
-            <Task />
-          </List>
+function Form(props) {
+    return (
+        <div>
+            <Title status={props.status} />
+            <Header placeholder_value={props.placeholder_value} onSubmit={props.changeTaskList}/>
+            <List>
+                <Task />
+                <Task />
+            </List>
         </div>
-      </main>
-  )
+    )
+}
+
+function App() {
+    const [list, setList] = useState([])
+
+    function addTask(e){
+        e.preventDefault()
+        setList(e.target.value)
+        console.log(list)
+    }
+
+    return (
+       <main className="main">
+          <div className="container">
+              <Form status={'high'} placeholder_value={'Добавить важных дел'} changeTaskList={addTask} />
+              <Form status={'low'} placeholder_value={'Добавить'} changeTaskList={addTask}/>
+          </div>
+       </main>
+    )
 }
 
 export default App
